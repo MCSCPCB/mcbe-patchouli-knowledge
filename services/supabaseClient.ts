f2c729223cb7cff20465a66db66653799e5e99f5
FoxFrisk <109194143+MCSCPCB@users.noreply.github.com>
2026-01-24 09:49:50 +0800

    Create supabaseClient.ts

---
+++ services/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+// 这些环境变量稍后会在 Vercel 后台配置
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase Environment Variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
