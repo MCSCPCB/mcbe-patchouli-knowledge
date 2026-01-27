7a6ab68f30b39111b97feae34c1cd18fa6463be2
FoxFrisk <109194143+MCSCPCB@users.noreply.github.com>
2026-01-27 17:20:10 +0800

    Update MarkdownElements.tsx

--- components/MarkdownElements.tsx
+++ components/MarkdownElements.tsx
@@ -518,7 +518,7 @@ const renderIDSNode = (node: IDSNode): string => {
      // 左下包通常是大长腿（走、辶），这里沿用标准逻辑，若有大头需求可在此添加
     return `<span class="relative w-full h-full block">
       <span class="absolute inset-0 flex items-end justify-start scale-[0.9] origin-bottom-left translate-x-[2px] translate-y-[0px]">${p1}</span>
-      <span class="absolute right-0 top-0 w-full h-full flex items-end justify-end scale-[0.6] origin-top-right pr-[5%] pt-[5%]">${p2}</span>
+      <span class="absolute right-0 top-0 w-full h-full flex items-end justify-end scale-[0.6] origin-top-right pr-[5%] pt-[5%] translate-y-[1px]">${p2}</span>
     </span>`;
   }
 
@@ -536,8 +536,8 @@ const renderIDSNode = (node: IDSNode): string => {
   // 2. ⿱ 上下结构 - 优化：增加垂直方向压扁 (scale-y-0.85)，防止过长
   if (val === '⿱') {
     return `<span class="relative w-full h-full block">
-      <span class="absolute top-0 left-0 w-full h-[50%] flex items-start justify-center scale-y-[0.3] scale-x-[0.9] origin-top translate-y-[-3px]">${p1}</span>
-      <span class="absolute bottom-0 left-0 w-full h-[50%] flex items-end justify-end scale-y-[0.55] scale-x-[0.9] origin-bottom translate-y-[3px]">${p2}</span>
+      <span class="absolute top-0 left-0 w-full h-[50%] flex items-start justify-center scale-y-[0.3] scale-x-[0.9] origin-top translate-y-[3px]">${p1}</span>
+      <span class="absolute bottom-0 left-0 w-full h-[50%] flex items-end justify-end scale-y-[0.55] scale-x-[0.9] origin-bottom translate-y-[-3px]">${p2}</span>
     </span>`;
   }
 
@@ -573,7 +573,7 @@ const renderIDSNode = (node: IDSNode): string => {
   if (val === '⿷') { 
     return `<span class="relative w-full h-full block">
       <span class="absolute inset-0 flex items-end justify-start scale-[0.9] origin-bottom-left translate-x-[2px] translate-y-[0px]">${p1}</span>
-      <span class="absolute inset-0 flex items-end justify-center scale-[0.5] translate-x-[15%] translate-y-[3px]">${p2}</span>
+      <span class="absolute inset-0 flex items-end justify-center scale-[0.5] translate-x-[15%] translate-y-[1px]">${p2}</span>
     </span>`;
   }
 
